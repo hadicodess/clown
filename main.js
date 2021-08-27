dbd34131e60c9cf2de4ef81e9802a225420e9539
hadicodess <85850834+hadicodess@users.noreply.github.com>
2021-08-26 22:20:23 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+nose_x = 0; 
+nose_y = 0; 
+
+function preload(){
+} 
+function setup(){
+    canvas = createCanvas(300, 300); 
+    canvas.center(); 
+    video = createCapture(VIDEO); 
+    video.size(300, 300); 
+    video.hide(); 
+    poseNet = ml5.poseNet(video, modelLoaded); 
+    poseNet.on('pose', gotPoses); } 
+function modelloaded() 
+{
+    console.log('PoseNet is inaitalizied '); 
+} 
+function gotPoses(results)
+{
+    if(results.length > 0) 
+    {
+        console.log(results); 
+        console.log("nose_x ="+ results[0].pose.nose.x);
+        console.log("nose_y ="+ results[0].pose.nose.y);
+    }
+}
+function draw(){ 
+    image(video, 0, 0, 300, 300); 
+} 
+
+function take_snapshot(){
+    save('myFillterImage.png'); 
+} 
